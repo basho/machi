feed231d5e78b4fc2f834bb033a37c03cc85ebc5
Scott Lystig Fritchie <slfritchie@snookles.com>
2014-02-16 20:32:08 +0900

    Move EUnit test code to test subdir

--- prototype/corfurl/include/corfurl.hrl
+++ prototype/corfurl/include/corfurl.hrl
@@ -18,6 +18,21 @@
 %%
 %% -------------------------------------------------------------------
 
+-type flu_name() :: atom().
+-type flu() :: pid() | flu_name().
+-type flu_chain() :: [flu()].
+
+-record(range, {
+          pn_start :: non_neg_integer(),            % start page number
+          pn_end :: non_neg_integer(),              % end page number
+          chains :: [flu_chain()]
+         }).
+
+-record(proj, {                                 % Projection
+          epoch :: non_neg_integer(),
+          r :: [#range{}]
+         }).
+
 %% 1 byte  @ offset 0: 0=unwritten, 1=written, 2=trimmed, 255=corrupt? TODO
 %% 8 bytes @ offset 1: logical page number
 %% P bytes @ offset 9: page data
