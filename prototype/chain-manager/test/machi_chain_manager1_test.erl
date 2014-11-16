07e477598aa0d71f6af02fa4b79cfd584227e2ab
Scott Lystig Fritchie <slfritchie@snookles.com>
2014-11-16 14:44:24 +0900

    OK, asymmetric network partition handling is stable. Time for next stage.
    Sketch:
    
    1. Exchange the roles of A30 and A20
    
    2. Query each participants' public & private stores, use them for A20 and A30
    
    3. Determine if there's flapping *before* calculating a new latest.
    
    3b. In flapping_i props, keep non-flapping UPI+Repairing and Down list.
          When calculating unique UPI+Repairing history and the unique Down lists history, use the non-flapping version if it exists.
    
    grrr/alt idea/bogus??.... Change the calculation of the unique UPIs and unique down lists. If the UPI & Down list looks the same as last time, and last time we were flapping, then we're still flapping.
    
    4a. Calculate a proposal using the standard Down info.
    4b. If there is flapping, then use Down <- All_Hosed instead of calculated down.
    
    5. If 4b's proposal is used ('cause flapping exists), preserve the non-flapping histories from 4a in each new proposal

--- prototype/chain-manager/test/machi_chain_manager1_test.erl
+++ prototype/chain-manager/test/machi_chain_manager1_test.erl
@@ -42,6 +42,95 @@
 -include_lib("eunit/include/eunit.hrl").
 -compile(export_all).
 
+unanimous_report(Namez) ->
+    UniquePrivateEs =
+        lists:usort(lists:flatten(
+                      [machi_flu0:proj_list_all(FLU, private) ||
+                          {_FLUName, FLU} <- Namez])),
+    [unanimous_report(Epoch, Namez) || Epoch <- UniquePrivateEs].
+
+unanimous_report(Epoch, Namez) ->
+    Projs = [{FLUName, case machi_flu0:proj_read(FLU, Epoch, private) of
+                           {ok, T} -> T;
+                           _Else   -> not_in_this_epoch
+                       end} || {FLUName, FLU} <- Namez],
+    UPI_R_Sums = [{Proj#projection.upi, Proj#projection.repairing,
+                   Proj#projection.epoch_csum} ||
+                     {_FLUname, Proj} <- Projs,
+                     is_record(Proj, projection)],
+    UniqueUPIs = lists:usort([UPI || {UPI, _Repairing, _CSum} <- UPI_R_Sums]),
+    Res =
+        [begin
+             case lists:usort([CSum || {U, _Repairing, CSum} <- UPI_R_Sums,
+                                       U == UPI]) of
+                 [_1CSum] ->
+                     %% Yay, there's only 1 checksum.  Let's check
+                     %% that all FLUs are in agreement.
+                     {UPI, Repairing, _CSum} =
+                         lists:keyfind(UPI, 1, UPI_R_Sums),
+                     %% TODO: make certain that this subtlety doesn't get
+                     %%       last in later implementations.
+
+                     %% So, this is a bit of a tricky thing.  If we're at
+                     %% upi=[c] and repairing=[a,b], then the transition
+                     %% (eventually!) to upi=[c,a] does not currently depend
+                     %% on b being an active participant in the repair.
+                     %%
+                     %% Yes, b's state is very important for making certain
+                     %% that all repair operations succeed both to a & b.
+                     %% However, in this simulation, we only consider that
+                     %% the head(Repairing) is sane.  Therefore, we use only
+                     %% the "HeadOfRepairing" in our considerations here.
+                     HeadOfRepairing = case Repairing of
+                                          [H_Rep|_] ->
+                                              [H_Rep];
+                                          _ ->
+                                              []
+                                      end,
+                     Tmp = [{FLU, case proplists:get_value(FLU, Projs) of
+                                      P when is_record(P, projection) ->
+                                          P#projection.epoch_csum;
+                                      Else ->
+                                          Else
+                                  end} || FLU <- UPI ++ HeadOfRepairing],
+                     case lists:usort([CSum || {_FLU, CSum} <- Tmp]) of
+                         [_] ->
+                             {agreed_membership, {UPI, Repairing}};
+                         Else2 ->
+                             {not_agreed, {UPI, Repairing}, Else2}
+                     end;
+                 _Else ->
+                     {UPI, not_unique, Epoch, _Else}
+             end
+         end || UPI <- UniqueUPIs],
+    AgreedResUPI_Rs = [UPI++Repairing ||
+                          {agreed_membership, {UPI, Repairing}} <- Res],
+    Tag = case lists:usort(lists:flatten(AgreedResUPI_Rs)) ==
+               lists:sort(lists:flatten(AgreedResUPI_Rs)) of
+              true ->
+                  ok_disjoint;
+              false ->
+                  bummer_NOT_DISJOINT
+          end,
+    {Epoch, {Tag, Res}}.
+
+all_reports_are_disjoint(Report) ->
+    [] == [X || {_Epoch, Tuple}=X <- Report,
+                element(1, Tuple) /= ok_disjoint].
+
+extract_chains_relative_to_flu(FLU, Report) ->
+    {FLU, [{Epoch, UPI, Repairing} ||
+              {Epoch, {ok_disjoint, Es}} <- Report,
+              {agreed_membership, {UPI, Repairing}} <- Es,
+              lists:member(FLU, UPI) orelse lists:member(FLU, Repairing)]}.
+
+chain_to_projection(MyName, Epoch, UPI_list, Repairing_list, All_list) ->
+    ?MGR:make_projection(Epoch, MyName, All_list,
+                         All_list -- (UPI_list ++ Repairing_list),
+                         UPI_list, Repairing_list, []).
+
+-ifndef(PULSE).
+
 smoke0_test() ->
     machi_partition_simulator:start_link({1,2,3}, 50, 50),
     {ok, FLUa} = machi_flu0:start_link(a),
@@ -137,95 +226,6 @@ nonunanimous_setup_and_fix_test() ->
         ok = machi_flu0:stop(FLUb),
         ok = machi_partition_simulator:stop()
     end.
-
-unanimous_report(Namez) ->
-    UniquePrivateEs =
-        lists:usort(lists:flatten(
-                      [machi_flu0:proj_list_all(FLU, private) ||
-                          {_FLUName, FLU} <- Namez])),
-    [unanimous_report(Epoch, Namez) || Epoch <- UniquePrivateEs].
-
-unanimous_report(Epoch, Namez) ->
-    Projs = [{FLUName, case machi_flu0:proj_read(FLU, Epoch, private) of
-                           {ok, T} -> T;
-                           _Else   -> not_in_this_epoch
-                       end} || {FLUName, FLU} <- Namez],
-    UPI_R_Sums = [{Proj#projection.upi, Proj#projection.repairing,
-                   Proj#projection.epoch_csum} ||
-                     {_FLUname, Proj} <- Projs,
-                     is_record(Proj, projection)],
-    UniqueUPIs = lists:usort([UPI || {UPI, _Repairing, _CSum} <- UPI_R_Sums]),
-    Res =
-        [begin
-             case lists:usort([CSum || {U, _Repairing, CSum} <- UPI_R_Sums,
-                                       U == UPI]) of
-                 [_1CSum] ->
-                     %% Yay, there's only 1 checksum.  Let's check
-                     %% that all FLUs are in agreement.
-                     {UPI, Repairing, _CSum} =
-                         lists:keyfind(UPI, 1, UPI_R_Sums),
-                     %% TODO: make certain that this subtlety doesn't get
-                     %%       last in later implementations.
-
-                     %% So, this is a bit of a tricky thing.  If we're at
-                     %% upi=[c] and repairing=[a,b], then the transition
-                     %% (eventually!) to upi=[c,a] does not currently depend
-                     %% on b being an active participant in the repair.
-                     %%
-                     %% Yes, b's state is very important for making certain
-                     %% that all repair operations succeed both to a & b.
-                     %% However, in this simulation, we only consider that
-                     %% the head(Repairing) is sane.  Therefore, we use only
-                     %% the "HeadOfRepairing" in our considerations here.
-                     HeadOfRepairing = case Repairing of
-                                          [H_Rep|_] ->
-                                              [H_Rep];
-                                          _ ->
-                                              []
-                                      end,
-                     Tmp = [{FLU, case proplists:get_value(FLU, Projs) of
-                                      P when is_record(P, projection) ->
-                                          P#projection.epoch_csum;
-                                      Else ->
-                                          Else
-                                  end} || FLU <- UPI ++ HeadOfRepairing],
-                     case lists:usort([CSum || {_FLU, CSum} <- Tmp]) of
-                         [_] ->
-                             {agreed_membership, {UPI, Repairing}};
-                         Else2 ->
-                             {not_agreed, {UPI, Repairing}, Else2}
-                     end;
-                 _Else ->
-                     {UPI, not_unique, Epoch, _Else}
-             end
-         end || UPI <- UniqueUPIs],
-    AgreedResUPI_Rs = [UPI++Repairing ||
-                          {agreed_membership, {UPI, Repairing}} <- Res],
-    Tag = case lists:usort(lists:flatten(AgreedResUPI_Rs)) ==
-               lists:sort(lists:flatten(AgreedResUPI_Rs)) of
-              true ->
-                  ok_disjoint;
-              false ->
-                  bummer_NOT_DISJOINT
-          end,
-    {Epoch, {Tag, Res}}.
-
-all_reports_are_disjoint(Report) ->
-    [] == [X || {_Epoch, Tuple}=X <- Report,
-                element(1, Tuple) /= ok_disjoint].
-
-extract_chains_relative_to_flu(FLU, Report) ->
-    {FLU, [{Epoch, UPI, Repairing} ||
-              {Epoch, {ok_disjoint, Es}} <- Report,
-              {agreed_membership, {UPI, Repairing}} <- Es,
-              lists:member(FLU, UPI) orelse lists:member(FLU, Repairing)]}.
-
-chain_to_projection(MyName, Epoch, UPI_list, Repairing_list, All_list) ->
-    ?MGR:make_projection(Epoch, MyName, All_list,
-                         All_list -- (UPI_list ++ Repairing_list),
-                         UPI_list, Repairing_list, []).
-
--ifndef(PULSE).
 
 short_doc() ->
 "
@@ -360,7 +360,10 @@ convergence_demo_test(_) ->
                                                        S_min, S_max_rand,
                                                        M_name, All_list),
                                                 _ = ?MGR:test_react_to_env(MMM),
-                                                timer:sleep(S_max - Elapsed)
+                                                %% Be more unfair by not
+                                                %% sleeping here.
+                                                %% timer:sleep(S_max - Elapsed),
+                                                Elapsed
                                             end || _ <- lists:seq(1, Iters)],
                                            Parent ! done
                                    end) || {M_name, MMM} <- [{a, Ma},
@@ -381,18 +384,25 @@ convergence_demo_test(_) ->
                                    X /= A],
       AllPartitionCombinations = XandYs1 ++ XandYs2,
       ?D({?LINE, length(AllPartitionCombinations)}),
+
+      machi_partition_simulator:reset_thresholds(10, 50),
+      io:format(user, "\nLet loose the dogs of war!\n", []),
+      DoIt(30, 0, 0),
       [begin
-           machi_partition_simulator:reset_thresholds(10, 50),
-           io:format(user, "\nLet loose the dogs of war!\n", []),
-           DoIt(30, 0, 0),
+           %% machi_partition_simulator:reset_thresholds(10, 50),
+           %% io:format(user, "\nLet loose the dogs of war!\n", []),
+           %% DoIt(30, 0, 0),
            machi_partition_simulator:always_these_partitions(Partition),
            io:format(user, "\nSET partitions = ~w.\n", [Partition]),
-           DoIt(40, 10, 100),
+           [DoIt(50, 10, 100) || _ <- [1,2] ],
            true = private_projections_are_stable(Namez, DoIt),
            io:format(user, "\nSweet, we converged to a stable state.\n", []),
            timer:sleep(1000),
            ok
        end || Partition <- AllPartitionCombinations],
+       %% end || Partition <- [ [{a,b},{b,a}, {a,c},{c,a}, {a,d},{d,a}, {b,c}],
+       %%                       [{a,b},{b,a}, {a,c},{c,a}, {a,d},{d,a}, {c,d}] ] ],
+       %% exit(end_experiment),
 
       io:format(user, "\nSET partitions = []\n", []),
       io:format(user, "We should see convergence to 1 correct chain.\n", []),
