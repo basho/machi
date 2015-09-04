42aeecd9dbf3882330f2a7091de900f85f3769a1
Scott Lystig Fritchie <slfritchie@snookles.com>
2015-09-04 15:23:48 +0900

    Fix machi_projection_store_test error

--- src/machi_projection_store.erl
+++ src/machi_projection_store.erl
@@ -306,11 +306,11 @@ do_proj_write3(ProjType, #projection_v1{epoch_number=Epoch,
             if CurEpoch == Epoch, CurCSum == CSum ->
                     do_proj_write4(ProjType, Proj, Path, Epoch, S);
                true ->
-                    io:format(user, "OUCH: on disk: ~w\n", [machi_projection:make_summary(binary_to_term(Bin))]),
-                    io:format(user, "OUCH: clobber: ~w\n", [machi_projection:make_summary(Proj)]),
-                    io:format(user, "OUCH: clobber: ~p\n", [Proj#projection_v1.dbg2]),
-                    %% {{error, written}, S}
-                    {{error, written, CurEpoch, Epoch, CurCSum, CSum}, S}
+                    %% io:format(user, "OUCH: on disk: ~w\n", [machi_projection:make_summary(binary_to_term(Bin))]),
+                    %% io:format(user, "OUCH: clobber: ~w\n", [machi_projection:make_summary(Proj)]),
+                    %% io:format(user, "OUCH: clobber: ~p\n", [Proj#projection_v1.dbg2]),
+                    %% {{error, written, CurEpoch, Epoch, CurCSum, CSum}, S}
+                    {{error, written}, S}
             end;
         {error, enoent} ->
             do_proj_write4(ProjType, Proj, Path, Epoch, S);
