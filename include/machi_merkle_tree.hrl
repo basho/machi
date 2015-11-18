8d3f631d84cc732e4064dcb65929bc3e802dc4bc
Mark Allen <mrallen1@yahoo.com>
2015-11-17 22:07:28 -0600

    Oops. Accidentally left this out.

---
+++ include/machi_merkle_tree.hrl
@@ -0,0 +1,20 @@
+%% machi merkle tree records
+
+-record(naive, {
+          chunk_size = 1048576   :: pos_integer(), %% default 1 MB
+          recalc = true          :: boolean(),
+          root                   :: 'undefined' | binary(),
+          lvl1 = []              :: [ binary() ],
+          lvl2 = []              :: [ binary() ],
+          lvl3 = []              :: [ binary() ],
+          leaves = []            :: [ { Offset :: pos_integer(),
+                                        Size :: pos_integer(),
+                                        Csum :: binary()} ]
+         }).
+
+-record(mt, {
+          filename               :: string(),
+          tree                   :: #naive{}|merklet:tree(),
+          backend = 'merklet'    :: 'naive'|'merklet'
+         }).
+
